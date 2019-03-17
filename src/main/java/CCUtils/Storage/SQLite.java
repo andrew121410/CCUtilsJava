ce5d5a230e7f9b94776b1f41b90a034eccb7abd3
andrew121410 <andrewcherring@gmail.com>
2019-03-17 14:11:25 -0400

    SQLite

---
+++ src/main/java/CCUtils/Storage/SQLite.java
@@ -0,0 +1,127 @@
+package CCUtils.Storage;
+
+import java.sql.*;
+
+public class SQLite {
+
+    private String dbName;
+
+    private Connection connection;
+
+    public SQLite(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public void Connect() {
+        try {
+            Class.forName("org.sqlite.JDBC").newInstance();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        final String url =
+                "jdbc:sqlite:" + dbName + ".db";
+        try {
+            this.connection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Disconnect() {
+        try {
+            if (!this.connection.isClosed() && this.connection != null) {
+                this.connection.close();
+                System.out.println(
+                        "[SQLITE] The connection to the MySQL server was successfully disconnected!");
+            } else {
+                System.out.println("[SQLITE] The connection is already disconnected!");
+            }
+        } catch (SQLException e3) {
+            System.out.println("[SQLITE] Error 03");
+            System.out.println("[SQLITE] There was an error while disconnecting!");
+            e3.printStackTrace();
+        }
+    }
+
+    public boolean isConnected() {
+        try {
+            return !this.connection.isClosed();
+        } catch (SQLException e2) {
+            System.out.println("[SQLITE] Error 02");
+            System.out.println("[SQLITE] An error occurred while connecting!");
+            e2.printStackTrace();
+        }
+        return false;
+    }
+
+    public ResultSet GetResult(String command) {
+        try {
+            if (this.connection.isClosed()) {
+                this.Connect();
+            }
+
+            Statement st = this.connection.createStatement();
+            st.executeQuery(command);
+            ResultSet rs = st.getResultSet();
+            return rs;
+
+        } catch (SQLException e4) {
+            System.out.println("[SQLITE] Error 04");
+            System.out
+                    .println("[SQLITE] An error occurred while executing the command!");
+            e4.printStackTrace();
+        }
+        return null;
+    }
+
+    public ResultSet GetResultPreparedStatement(String command) {
+        try {
+            if (this.connection.isClosed()) {
+                this.Connect();
+            }
+            PreparedStatement pst = this.connection.prepareStatement(command);
+            pst.executeQuery();
+            ResultSet rs = pst.getResultSet();
+            return rs;
+
+        } catch (SQLException e4) {
+            System.out.println("[SQLITE] Error 04");
+            System.out
+                    .println("[SQLITE] An error occurred while executing the command!");
+            e4.printStackTrace();
+        }
+        return null;
+    }
+
+    public void ExecuteCommand(String command) {
+        try {
+            if (this.connection.isClosed()) {
+                this.Connect();
+            }
+            Statement st = this.connection.createStatement();
+            st.executeUpdate(command);
+        } catch (SQLException e4) {
+            System.out.println("[SQLITE] Error 04");
+            System.out
+                    .println("[SQLITE] An error occurred while executing the command!");
+            e4.printStackTrace();
+        }
+
+    }
+
+    public PreparedStatement ExecuteCommandPreparedStatement(String command) {
+        try {
+            if (this.connection.isClosed()) {
+                this.Connect();
+            }
+            PreparedStatement pst = this.connection.prepareStatement(command);
+            return pst;
+        } catch (SQLException e4) {
+            System.out.println("[SQLITE] Error 04");
+            System.out
+                    .println("[SQLITE] An error occurred while executing the command!");
+            e4.printStackTrace();
+        }
+        return null;
+    }
+}
