0adabacec9a85e2ae53fd96168413fef4f90f140
andrew121410 <andrewcherring@gmail.com>
2019-12-07 16:49:17 -0500

    PLEASE WORK.

--- src/main/java/CCUtils/Sockets/SocketChannels/Server/SimpleSocketChannelServer.java
+++ src/main/java/CCUtils/Sockets/SocketChannels/Server/SimpleSocketChannelServer.java
@@ -5,7 +5,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -26,55 +28,73 @@ public class SimpleSocketChannelServer {
         try {
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.configureBlocking(false);
-            serverSocketChannel.socket().bind(new InetSocketAddress("localhost", port));
+            serverSocketChannel.socket().bind(new InetSocketAddress(port));
 
             Selector selector = Selector.open();
-            int ops = serverSocketChannel.validOps();
-            serverSocketChannel.register(selector, ops, null);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
             while (true) {
                 selector.select();
                 Set<SelectionKey> selectionKeys = selector.selectedKeys();
-                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                Iterator<SelectionKey> keys = selectionKeys.iterator();
 
-                while (iterator.hasNext()) {
-                    SelectionKey selectionKey = iterator.next();
+                while (keys.hasNext()) {
+                    SelectionKey selectionKey = keys.next();
 
                     if (selectionKey.isAcceptable()) {
-                        SocketChannel socketChannel = serverSocketChannel.accept();
-                        socketChannel.configureBlocking(false);
-                        socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-                        System.out.println("Connection Accepted: " + socketChannel.getLocalAddress() + "\n");
-
+                        createChannel(serverSocketChannel, selectionKey);
                     } else if (selectionKey.isReadable()) {
-                        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                        ByteBuffer byteBuffer = ByteBuffer.allocate(256);
-                        socketChannel.read(byteBuffer);
-                        String message = new String(byteBuffer.array()).trim();
-
-                        JSONParser parser = new JSONParser();
-                        JSONObject json;
-                        try {
-                            json = (JSONObject) parser.parse(message);
-                        } catch (ParseException e) {
-                            System.out.println("NOT JSON");
-                            return;
-                        }
-                        if (json == null) return;
-
-                        String who = (String) json.get("WHO");
-                        CCUtils.Sockets.interfaces.ServerSocketHandler serverSocketHandler = this.serverSocketHandlerMap.get(who);
-                        if (serverSocketHandler != null) {
-                            serverSocketHandler.translate(json);
-                        } else System.out.println("ClientHandler -> WHO: " + who);
+                        doRead(selectionKey);
+                    } else if (selectionKey.isWritable()) {
+                        //TODO add write.
                     }
-
-                    iterator.remove();
+                    keys.remove();
                 }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    private void createChannel(ServerSocketChannel serverSocketChannel, SelectionKey selectionKey) throws IOException {
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selectionKey.selector(), SelectionKey.OP_READ); // selector pointing to READ operation
+    }
+
+    private void doRead(SelectionKey selectionKey) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        int read = socketChannel.read(byteBuffer);
+        if (read == -1) {
+            doClose(socketChannel);
+        } else {
+            String result = new String(byteBuffer.array()).trim();
+
+            JSONParser parser = new JSONParser();
+            JSONObject json;
+            try {
+                json = (JSONObject) parser.parse(result);
+            } catch (ParseException e) {
+                System.out.println("NOT JSON");
+                return;
+            }
+            if (json == null) return;
+
+            String who = (String) json.get("WHO");
+            CCUtils.Sockets.interfaces.ServerSocketHandler serverSocketHandler = this.serverSocketHandlerMap.get(who);
+            if (serverSocketHandler != null) {
+                serverSocketHandler.translate(json);
+            } else System.out.println("ClientHandler -> WHO: " + who);
+
+            selectionKey.interestOps(SelectionKey.OP_WRITE); // set mode to WRITE to send data
+        }
+    }
+
+    private void doClose(SocketChannel socketChannel) throws IOException {
+        SocketAddress remoteSocketAddress = socketChannel.getRemoteAddress();
+        System.out.println("Connection closed by client: " + remoteSocketAddress);
+        socketChannel.close();
     }
 
     public void registerHandler(String key, ServerSocketHandler serverSocketHandler) {
