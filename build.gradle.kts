9bc270d6d8489c91abd64a4a43dc4438e261b3d6
Andrew121410 <andrewcherring@gmail.com>
2024-05-02 22:52:51 -0400

    Gradle time

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1" // https://github.com/johnrengelman/shadow
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.andrew121410"
+version = "1.0"
+description = "CCUtils"
+
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
+
+tasks {
+    build {
+        dependsOn("shadowJar")
+        dependsOn("processResources")
+    }
+
+    jar {
+        enabled = false
+    }
+
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    shadowJar {
+        // Not needed because it's a library.
+        archiveFileName.set("CCUtilsJava.jar")
+
+        relocate("com.google", "com.andrew121410.ccutils.dependencies.google")
+        relocate("com.mysql", "com.andrew121410.ccutils.dependencies.mysql")
+        relocate("org.xerial", "com.andrew121410.ccutils.dependencies.sqlite")
+    }
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    api(libs.com.mysql.mysql.connector.j)
+    api(libs.org.xerial.sqlite.jdbc)
+    api(libs.com.google.guava.guava)
+    compileOnly(libs.org.projectlombok.lombok)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("shadow") {
+            artifact(tasks.named("shadowJar"))
+        }
+    }
+}
